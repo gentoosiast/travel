57c34823bff90a5741c3bf44707c1f0948c1c92c
Sergey Koksharov <gentoosiastic@gmail.com>
2022-07-25 10:15:23 +0300

    refactor: use ESLint to enforce Airbnb JS style guide

--- travel/app.js
+++ travel/app.js
@@ -1,175 +1,181 @@
-(function () {
-  // BURGER MENU
-  const burger = document.getElementById("burger");
-  const menu = document.querySelector(".header__menu");
-  const menuClose = document.querySelector(".menu__close");
-  const menuItems = document.querySelector(".menu__items");
-  const fadeout = document.querySelector(".fadeout-container");
-
-  burger.addEventListener("click", showMenu);
-  menuClose.addEventListener("click", hideMenu);
-  menuItems.addEventListener("click", handleMenuClick);
-  fadeout.addEventListener("click", hideMenu);
-
-  function showMenu() {
-    menu.classList.remove("header__menu_hidden");
-    fadeout.classList.add("fadeout-container_active");
-  }
-
-  function handleMenuClick(event) {
-    if (event.target.classList.contains("menu__link")) {
-      hideMenu();
-    }
-    if (event.target.classList.contains("menu__link_account")) {
-      showPopup();
-    }
-  }
-
-  function hideMenu() {
-    menu.classList.add("header__menu_hidden");
-    fadeout.classList.remove("fadeout-container_active");
-  }
+(() => {
+  // FADEOUT
+  const fadeout = document.querySelector('.fadeout-container');
 
   // SIGN-IN / REGISTER POPUP
-  const headerButton = document.querySelector(".header__button");
-  const popup = document.querySelector(".popup");
-  const signinPopup = document.querySelector(".popup__signin");
-  const registerPopup = document.querySelector(".popup__register");
+  const headerButton = document.querySelector('.header__button');
+  const popup = document.querySelector('.popup');
+  const signinPopup = document.querySelector('.popup__signin');
+  const registerPopup = document.querySelector('.popup__register');
   const signinForm = document.forms.signin;
   const registerForm = document.forms.register;
-  const signinLink = document.querySelector(".popup__link_signin");
-  const registerLink = document.querySelector(".popup__link_register");
-
-  headerButton.addEventListener("click", showPopup);
-  signinForm.addEventListener("submit", submitForm);
-  registerForm.addEventListener("submit", submitForm);
-  signinLink.addEventListener("click", switchPopup);
-  registerLink.addEventListener("click", switchPopup);
-  fadeout.addEventListener("click", closePopup);
+  const signinLink = document.querySelector('.popup__link_signin');
+  const registerLink = document.querySelector('.popup__link_register');
 
   function showPopup() {
-    document.body.classList.add("body-noscroll");
-    popup.classList.remove("popup_hidden");
-    fadeout.classList.add("fadeout-container_active");
+    document.body.classList.add('body-noscroll');
+    popup.classList.remove('popup_hidden');
+    fadeout.classList.add('fadeout-container_active');
   }
 
   function restorePopupSettings() {
-    signinPopup.classList.remove("popup__signin_hidden");
-    registerPopup.classList.add("popup__register_hidden");
-    popup.removeEventListener("transitionend", restorePopupSettings);
+    signinPopup.classList.remove('popup__signin_hidden');
+    registerPopup.classList.add('popup__register_hidden');
+    popup.removeEventListener('transitionend', restorePopupSettings);
   }
 
   function closePopup() {
-    document.body.classList.remove("body-noscroll");
-    popup.classList.add("popup_hidden");
-    fadeout.classList.remove("fadeout-container_active");
-    popup.addEventListener("transitionend", restorePopupSettings);
+    document.body.classList.remove('body-noscroll');
+    popup.classList.add('popup_hidden');
+    fadeout.classList.remove('fadeout-container_active');
+    popup.addEventListener('transitionend', restorePopupSettings);
   }
 
   function submitForm(event) {
+    /* eslint-disable no-alert */
     alert(
-      `Submitted data:\nEmail: ${event.target.elements.email.value}\nPassword: ${event.target.elements.password.value}`
+      `Submitted data:\nEmail: ${event.target.elements.email.value}\nPassword: ${event.target.elements.password.value}`,
     );
+    /* eslint-enable no-alert */
     closePopup();
   }
 
   function switchPopup() {
-    signinPopup.classList.toggle("popup__signin_hidden");
-    registerPopup.classList.toggle("popup__register_hidden");
+    signinPopup.classList.toggle('popup__signin_hidden');
+    registerPopup.classList.toggle('popup__register_hidden');
   }
+
+  headerButton.addEventListener('click', showPopup);
+  signinForm.addEventListener('submit', submitForm);
+  registerForm.addEventListener('submit', submitForm);
+  signinLink.addEventListener('click', switchPopup);
+  registerLink.addEventListener('click', switchPopup);
+  fadeout.addEventListener('click', closePopup);
+
+  // BURGER MENU
+  const burger = document.getElementById('burger');
+  const menu = document.querySelector('.header__menu');
+  const menuClose = document.querySelector('.menu__close');
+  const menuItems = document.querySelector('.menu__items');
+
+  function showMenu() {
+    menu.classList.remove('header__menu_hidden');
+    fadeout.classList.add('fadeout-container_active');
+  }
+
+  function hideMenu() {
+    menu.classList.add('header__menu_hidden');
+    fadeout.classList.remove('fadeout-container_active');
+  }
+
+  function handleMenuClick(event) {
+    if (event.target.classList.contains('menu__link')) {
+      hideMenu();
+    }
+    if (event.target.classList.contains('menu__link_account')) {
+      showPopup();
+    }
+  }
+
+  burger.addEventListener('click', showMenu);
+  menuClose.addEventListener('click', hideMenu);
+  menuItems.addEventListener('click', handleMenuClick);
+  fadeout.addEventListener('click', hideMenu);
 
   // SLIDER
-  const circleButtons = document.querySelectorAll(".circle-button");
-  const slider = document.querySelector(".carousel__slides");
-  const slides = document.getElementsByClassName("carousel__slide");
-  const prevArrow = document.querySelector(".carousel__arrow_prev");
-  const nextArrow = document.querySelector(".carousel__arrow_next");
-
+  const circleButtons = document.querySelectorAll('.circle-button');
+  const slider = document.querySelector('.carousel__slides');
+  const slides = document.getElementsByClassName('carousel__slide');
+  const prevArrow = document.querySelector('.carousel__arrow_prev');
+  const nextArrow = document.querySelector('.carousel__arrow_next');
   const numSlides = slides.length;
-  let shiftValue = getShiftValue();
-  let curIdx = 1; // 2nd slide
-
-  slides[curIdx - 1].addEventListener("click", shiftLeft);
-  slides[curIdx + 1].addEventListener("click", shiftRight);
-  prevArrow.addEventListener("click", shiftLeft);
-  nextArrow.addEventListener("click", shiftRight);
-  circleButtons.forEach((button, idx) =>
-    button.addEventListener("click", function () {
-      switchSlide(idx);
-    })
-  );
-
-  // recalculate shiftValue and slider position
-  function handleResize() {
-    slider.classList.remove("carousel__slides_transition");
-    shiftValue = getShiftValue();
-    setSliderPosition(curIdx);
-    slider.offsetHeight; // Trigger a reflow, flushing the CSS changes
-    slider.classList.add("carousel__slides_transition");
-  }
-
-  new ResizeObserver(handleResize).observe(slider);
 
   function getShiftValue() {
     const slideWidth = slides[0].offsetWidth;
     const gapWidth = Math.floor(
-      (slider.offsetWidth - slideWidth * numSlides) / (numSlides - 1)
+      (slider.offsetWidth - slideWidth * numSlides) / (numSlides - 1),
     );
     return slideWidth + gapWidth;
   }
+
+  let shiftValue = getShiftValue();
+  let curSlideIdx = 1; // 2nd slide
 
   function setSliderPosition(idx) {
     const shiftPosition = idx === 0 ? shiftValue : (idx - 1) * -shiftValue;
     slider.style.transform = `translateX(${shiftPosition}px)`;
   }
 
+  /* eslint-disable no-use-before-define */
   function switchSlide(idx) {
     // index out of bounds or same index as current
-    if (idx < 0 || idx > numSlides - 1 || curIdx === idx) {
+    if (idx < 0 || idx > numSlides - 1 || curSlideIdx === idx) {
       return;
     }
 
-    slides[curIdx].classList.remove("slide_active");
-    if (curIdx > 0) {
-      slides[curIdx - 1].removeEventListener("click", shiftLeft);
+    slides[curSlideIdx].classList.remove('slide_active');
+    if (curSlideIdx > 0) {
+      slides[curSlideIdx - 1].removeEventListener('click', shiftLeft);
     } else {
-      prevArrow.classList.remove("carousel__arrow_disabled");
+      prevArrow.classList.remove('carousel__arrow_disabled');
     }
-    if (curIdx < numSlides - 1) {
-      slides[curIdx + 1].removeEventListener("click", shiftRight);
+    if (curSlideIdx < numSlides - 1) {
+      slides[curSlideIdx + 1].removeEventListener('click', shiftRight);
     } else {
-      nextArrow.classList.remove("carousel__arrow_disabled");
+      nextArrow.classList.remove('carousel__arrow_disabled');
     }
-    circleButtons[curIdx].classList.remove("circle-button_active");
+    circleButtons[curSlideIdx].classList.remove('circle-button_active');
 
-    curIdx = idx;
-    slides[curIdx].classList.add("slide_active");
-    if (curIdx > 0) {
-      slides[curIdx - 1].addEventListener("click", shiftLeft);
+    curSlideIdx = idx;
+    slides[curSlideIdx].classList.add('slide_active');
+    if (curSlideIdx > 0) {
+      slides[curSlideIdx - 1].addEventListener('click', shiftLeft);
     } else {
-      prevArrow.classList.add("carousel__arrow_disabled");
+      prevArrow.classList.add('carousel__arrow_disabled');
     }
-    if (curIdx < numSlides - 1) {
-      slides[curIdx + 1].addEventListener("click", shiftRight);
+    if (curSlideIdx < numSlides - 1) {
+      slides[curSlideIdx + 1].addEventListener('click', shiftRight);
     } else {
-      nextArrow.classList.add("carousel__arrow_disabled");
+      nextArrow.classList.add('carousel__arrow_disabled');
     }
-    circleButtons[curIdx].classList.add("circle-button_active");
-    setSliderPosition(curIdx);
+    circleButtons[curSlideIdx].classList.add('circle-button_active');
+    setSliderPosition(curSlideIdx);
   }
+  /* eslint-enable no-use-before-define */
 
   function shiftLeft() {
-    switchSlide(curIdx - 1);
+    switchSlide(curSlideIdx - 1);
   }
 
   function shiftRight() {
-    switchSlide(curIdx + 1);
+    switchSlide(curSlideIdx + 1);
   }
 
+  slides[curSlideIdx - 1].addEventListener('click', shiftLeft);
+  slides[curSlideIdx + 1].addEventListener('click', shiftRight);
+  prevArrow.addEventListener('click', shiftLeft);
+  nextArrow.addEventListener('click', shiftRight);
+  circleButtons.forEach((button, idx) => {
+    button.addEventListener('click', () => switchSlide(idx));
+  });
+
+  // recalculate shiftValue and slider position
+  function handleResize() {
+    slider.classList.remove('carousel__slides_transition');
+    shiftValue = getShiftValue();
+    setSliderPosition(curSlideIdx);
+    /* eslint-disable no-unused-expressions */
+    slider.offsetHeight; // Trigger a reflow, flushing the CSS changes
+    /* eslint-enable no-unused-expressions */
+    slider.classList.add('carousel__slides_transition');
+  }
+
+  new ResizeObserver(handleResize).observe(slider);
+
   // INITIALIZATION ON DOCUMENT LOAD
-  globalThis.onload = function () {
-    // prevent flicker on page load
-    menu.style.display = "block";
-    popup.style.display = "block";
+  globalThis.onload = () => {
+    // undo 'display: none' (needed to prevent flicker on page load)
+    menu.style.display = 'block';
+    popup.style.display = 'block';
   };
 })();
